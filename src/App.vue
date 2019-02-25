15163842b929292585d68a579e073ee84ec32236
Jonathan Smith <jonathan@peoplesized.com>
2019-02-25 16:39:42 +0200

    clear out

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <thead>
+      <tr>
+        <th>Fn</th>
+      </tr>
+
+      </thead>
+
+      <tbody>
+      <tr>
+        <td>Super</td>
+      </tr>
+      </tbody>
+
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    components: {}
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "../node_modules/picnic/picnic.min.css";
 </style>
